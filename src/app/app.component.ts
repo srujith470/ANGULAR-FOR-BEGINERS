dc33cac76f00eaadc2b24852768bd063041db58c
srujith470 <srujith.reddy2012@gmail.com>
2018-12-25 10:35:57 -0500

    VIEWCHILD&AFTERVIEWINIT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import {COURSES} from '../db-data';
 import { Course } from './model/course';
+import { CourseCardComponent } from './course-card/course-card.component';
+import { Element } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +12,28 @@ import { Course } from './model/course';
 export class AppComponent {
 
   courses = COURSES;
-
   startDate = new Date()
-  onCourseSelected(course:Course){
-    console.log('app component clicked', course)
+
+  @ViewChild('cardRef1',{read:ElementRef})
+  card1: ElementRef
+
+  @ViewChild('courseImage')
+  courseImage: ElementRef
+
+  @ViewChild('container')
+  containerDiv: ElementRef
+
+  constructor(){
+    console.log('app component form constructor', this.card1)
   }
+
+  ngAfterViewInit(){
+    console.log('app component from lifecycle', this.card1)
+  }
+
+  onCourseSelected(course:Course){
+     console.log('app component clicked for image', this.courseImage)
+
+   }
 
 }
